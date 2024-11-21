21a8c8f8a85fa067cb5c423220b4dc5568cc0c1f
wambui <wambuistephen@zetech.ac.ke>
2024-11-21 21:10:26 +0300

    new

--- Gp/settings.py
+++ Gp/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -141,7 +141,7 @@ MEDIA_URL = '/media/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
