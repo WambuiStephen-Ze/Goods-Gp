53b197cbae01be91cc36a8f0873abf4462d2bf94
wambui <wambuistephen@zetech.ac.ke>
2024-11-21 17:54:23 +0300

    nice

--- Gp/settings.py
+++ Gp/settings.py
@@ -79,12 +79,13 @@ WSGI_APPLICATION = 'Gp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'good',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : BASE_DIR / 'db.sqlite3',
+        # 'NAME': 'good',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
